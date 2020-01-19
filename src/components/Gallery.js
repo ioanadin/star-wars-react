5031ae81788c0b0d0fac2d26c57fa34aa457d805
Ioana Din <ioanadin06@gmail.com>
2020-01-19 22:24:13 +0200

    rendering movies

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,13 +2,48 @@ import React from 'react';
 import Movie from './Movie';
 import getMovies from '../util/swapi';
 
-function Gallery() {
-    let apiResponse = getMovies()
-    console.log(apiResponse);
-    return <div>
-        {/* <Movie title={apiResponse.results[0].title} /> */}
-        <Movie />
-    </div>
+// function Gallery() {
+//     let apiResponse = getMovies()
+//     console.log(apiResponse);
+//     return <div>
+//         {/* <Movie title={apiResponse.results[0].title} /> */}
+//         <Movie />
+//     </div>
+// }
+
+
+class Gallery extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoading: true,
+            movieDetails: null
+        };
+    }
+    componentDidMount() {
+        this.fetchMovieData();
+    }
+
+    async fetchMovieData() {
+        const movieDetails = await getMovies();
+        this.setState({ movieDetails: movieDetails, isLoading: false });
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return <div className='movie-item'>
+                <p> Loading movies...</p>
+            </div>
+        }
+        else {
+            let swMovies = [];
+            for (let i = 0; i < this.state.movieDetails.count; i++) {
+                swMovies.push(<Movie details={this.state.movieDetails.results[i]} />)
+            }
+            return swMovies;
+        }
+
+    }
 }
 
 export default Gallery;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,61 +4,18 @@ import swapi from '../util/swapi';
 import getMovies from '../util/swapi';
 
 
-// function Movie(props) {
-//     return (
-//         <div>
-//             <h1>Star Wars</h1>
-//         </div>
-
-//     )
-// }
 class Movie extends React.Component {
     constructor(props) {
         super(props)
-
-        this.state = {
-            title: null,
-            isLoading: true,
-        };
     }
-
-    componentDidMount() {
-        this.fetchMovieData();
-    }
-
-
-    preloadImage(movieDetails) {
-        const image = new Image();
-        image.src = movieDetails.url;
-        image.onload = () => {
-            this.setState({ movieDetails, isLoading: false });
-        }
-    };
-
-    async fetchMovieData() {
-        this.setState({ isLoading: true });
-        const movieDetails = await getMovies();
-        this.setState({ title: movieDetails.results[0].title, isLoading: false });
-    }
-
     render() {
-        if (this.state.isLoading) {
-            return <div className='movie-item'>
-                <p> Loading...</p>
-            </div>
-        }
-        else {
-            return <div className='movie-item'>
-                {this.state.title}
-            </div>
-
-
-        }
-
+        return <div className='movie-item'>
+            {this.props.details.title}
+            <br />
+            {this.props.details.episode_id}
+            <br />
+            {this.props.details.release_date}
+        </div>
     }
-
-
-
-
 }
 export default Movie;
