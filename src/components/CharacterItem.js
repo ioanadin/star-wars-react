ddea3c20c498d764e8c82de4db3b8a583f3d6afd
ioanadin <47700901+ioanadin@users.noreply.github.com>
2020-01-29 22:26:20 +0200

    navigate to character details

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 class CharacterItem extends React.Component {
+    navigateToCharacterDetails = () => {
+        this.props.history.push("/character", this.props.character);
+    }
 
     render() {
         return (
-            <div>
+            <div onClick={this.navigateToCharacterDetails}>
                 <p>{this.props.character.name}</p>
             </div>
         );
     }
-
 }
 
 export default withRouter(CharacterItem);
