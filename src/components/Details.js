ff0b53f49848b1343f5114f2520b5972b52d213f
ioanadin <47700901+ioanadin@users.noreply.github.com>
2020-01-30 21:54:20 +0200

    episode name and id, crawl bottom border

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withRouter, Redirect } from 'react-router-dom';
 import CharacterGallery from './CharacterGallery';
 import OpeningCrawl from './OpeningCrawl';
+import './Details.css';
 
 function Details(props) {
     const details = props.history.location.state;
@@ -13,8 +14,8 @@ function Details(props) {
     return (
         <div>
             <OpeningCrawl text={details.opening_crawl} />
-            <h1>{details.title}</h1>
-            <h3>Episode {details.episode_id}</h3>
+            <h1 className="episode-title">{details.title}</h1>
+            <h3 className="episode-id">Episode {details.episode_id}</h3>
             <div><CharacterGallery urls={details.characters} /></div>
         </div>
     );
