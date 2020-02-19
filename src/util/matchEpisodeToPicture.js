b622429f0c98dd8fa8bf9293d930276384cc59ba
Ioana Din <ioanadin06@gmail.com>
2020-02-19 22:15:10 +0200

    movie covers moved to static folder

--- src/util/matchEpisodeToPicture.js
+++ src/util/matchEpisodeToPicture.js
@@ -1,27 +1,19 @@
-import episode1url from '../sw-covers/Star-Wars-Episode-I-The-Phantom-Menace.jpg';
-import episode2url from '../sw-covers/Star-Wars-Episode-II-Attack-Of-The-Clones.jpg';
-import episode3url from '../sw-covers/Star-Wars-Episode-III-Revenge-Of-The-Sith.jpg';
-import episode4url from '../sw-covers/Star-Wars-Episode-IV-A-New-Hope.jpg';
-import episode5url from '../sw-covers/Star-Wars-Episode-V-The-Empire-Strikes-Back.jpg';
-import episode6url from '../sw-covers/Star-Wars-Episode-VI-Return-Of-The-Jedi.jpg';
-import episode7url from '../sw-covers/Star-Wars-Episode-VII-The-Force-Awakens.jpg';
-
 function matchEpisodeToPicture(episodeId) {
     switch (episodeId) {
         case 1:
-            return episode1url;
+            return '/Star-Wars-Episode-I-The-Phantom-Menace.jpg';
         case 2:
-            return episode2url;
+            return '/Star-Wars-Episode-II-Attack-Of-The-Clones.jpg';;
         case 3:
-            return episode3url;
+            return '/Star-Wars-Episode-III-Revenge-Of-The-Sith.jpg';
         case 4:
-            return episode4url;
+            return '/Star-Wars-Episode-IV-A-New-Hope.jpg';
         case 5:
-            return episode5url;
+            return '/Star-Wars-Episode-V-The-Empire-Strikes-Back.jpg';
         case 6:
-            return episode6url;
+            return '/Star-Wars-Episode-VI-Return-Of-The-Jedi.jpg';
         case 7:
-            return episode7url;
+            return '/Star-Wars-Episode-VII-The-Force-Awakens.jpg';
         default:
             return null;
     }
